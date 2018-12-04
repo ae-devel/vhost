3ad19461c0da66138c46ed7108fa109fbe411cf1
ae-devel <vlasov@web-ae.ru>
2018-12-04 07:47:11 +0000

    Changes to be committed: 	modified:   classes/CommonFunctions.php 	modified:   controllers/IndexController.php
     Untracked files:
    	tpl/indextemplate/

--- classes/CommonFunctions.php
+++ classes/CommonFunctions.php
@@ -2,6 +2,9 @@
 
 	class CommonFunctions {
 		
-		
+		public static function displayError( $ErrorMessage ) {
+			echo '<div style="width: 100%; height: 500px; padding: 20px; background: #000; color: #FFF; border-top: 3px solid yellow; position:fixed; left: 0; bottom: 0; z-index: 9999; overflow-y: scroll;">' . $ErrorMessage . '</div>';
+			die();
+		}
 		
 	}
--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,3 +1,20 @@
 <?php
 
-	echo strtoupper( $_SERVER[ 'SERVER_NAME' ] ) . ', INDEX CTONROLLER';
+	try {
+		
+		$Data = array();
+
+
+		$Data = array( 
+			'page_title' => 'My little world',
+			'server_name' => $_SERVER[ 'SERVER_NAME' ],
+			'template_path' => '/tpl/indextemplate',
+		);
+		
+		include_once PATH_TPL_DIR . '/indextemplate/index.tpl';
+
+	} catch( Exception $Ex ) {
+		
+		CommonFunctions::displayError( $Ex->getMessage() );
+		
+	}
