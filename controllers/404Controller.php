9c3f0d1cf74c411ed394abd11d683a10de390786
ae-devel <vlasov@web-ae.ru>
2019-03-18 09:46:34 +0000

    Restoration

--- controllers/404Controller.php
+++ controllers/404Controller.php
@@ -1,3 +1,19 @@
 <?php
 
-	echo '404, Not found';
+	try {
+		
+		$Data = array();
+
+
+		$Data = array( 
+			'page_title' => '404, Page not found',
+			'template_path' => '/tpl'
+		);
+		
+		include_once PATH_TPL_DIR . '/system/404.tpl';
+
+	} catch( Exception $Ex ) {
+		
+		CommonFunctions::displayError( $Ex->getMessage() );
+		
+	}
